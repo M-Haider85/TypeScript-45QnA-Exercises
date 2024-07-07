9d5f0ec3458e11c5618a40b0299ea078e8abc832
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-08 02:58:30 +0500

    Add files via upload

---
+++ Ex-10.ts
@@ -0,0 +1,4 @@
+console.log(5 + 3); // In this program we are doing Addition
+console.log(10 - 2); // In this program we are doing Subtraction
+console.log(4 * 2); // In this program we are doing Multiplication
+console.log(16 / 2); // In this program we are doing Division
