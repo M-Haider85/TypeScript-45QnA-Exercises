af60ccd8e55296f2ea88c0942e21af4b748e2fa6
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-08 03:00:06 +0500

    Add files via upload

---
+++ Ex-13.ts
@@ -0,0 +1,6 @@
+let favouriteCar:string[]=['BMW','Porsche','Benz','Ferrari','Tesla','Mercedes']
+for(let a=0;a<favouriteCar.length;a++){
+    console.log(`I would like to own a ${favouriteCar[a]}`);
+    
+    
+}
