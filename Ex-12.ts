ca3fac55c193e6a9c258ff5c1cf68d96fabb8926
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-08 02:59:45 +0500

    Add files via upload

---
+++ Ex-12.ts
@@ -0,0 +1,5 @@
+let friendsName=["Ali","Umar","Ibrahim","Uzair","Imran"]
+for(let a=0;a<friendsName.length;a++){
+    console.log("Hello, " +friendsName[a]+ "! I hope you are doing well.");
+    
+}
