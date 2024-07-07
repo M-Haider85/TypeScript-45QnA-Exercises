6edf878296f67d91a77dd1dd83e14af8866f097f
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-08 03:00:47 +0500

    Add files via upload

---
+++ Ex-14.ts
@@ -0,0 +1,4 @@
+let guestList:string[]=['Bilal','Uzair','Taha','Mugheerah','Ali']
+for(let i=0;i<guestList.length;i++){
+    console.log(`Hello ${guestList[i]}, I would like to invite you for dinner.`);  
+}
