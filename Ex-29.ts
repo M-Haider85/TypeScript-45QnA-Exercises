ab91379c37df63bc0d1e76b16df0a1d2017ef3a2
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-09 12:23:01 +0500

    Add files via upload

---
+++ Ex-29.ts
@@ -0,0 +1,26 @@
+let favorite_fruits:string[]=["Watermelon","Mango","Banana","Lychee","Peach","Apple"]
+
+// Check if 'Apple' is in the array
+if (favorite_fruits.includes('Apple')) {
+    console.log("I really like Apples!");
+}
+
+// Check if 'Banana' is in the array
+if (favorite_fruits.includes('Banana')) {
+    console.log("I really like Bananas!");
+}
+
+// Check if 'Mango' is in the array
+if (favorite_fruits.includes('Mango')) {
+    console.log("I really like mangoes!");
+}
+
+// Check if 'orange' is in the array
+if (favorite_fruits.includes('orange')) {
+    console.log("I really like oranges!");
+}
+
+// Check if 'grape' is in the array
+if (favorite_fruits.includes('grape')) {
+    console.log("I really like grapes!");
+}
