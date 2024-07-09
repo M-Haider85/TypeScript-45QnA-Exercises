3937922a5e97e332a934a63cb1f345071197bcfe
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-09 12:30:39 +0500

    Add files via upload

---
+++ Ex-41.ts
@@ -0,0 +1,9 @@
+let magicians:string[]=["Asif Ali Zardari","Nawaz Sharif","Asim Muneer"]
+function show_Magicians(magicians:string[]){
+    for(let i=0; i<magicians.length; i++){
+        console.log(magicians[i]);
+        
+    }
+    
+}
+show_Magicians(magicians)
