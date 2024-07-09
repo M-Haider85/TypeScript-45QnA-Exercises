0a57b107b721080878f9e983b262837f35dfea65
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-09 12:20:34 +0500

    Add files via upload

---
+++ Ex-25.ts
@@ -0,0 +1,11 @@
+let alienColor="Green";
+if(alienColor=="Green"){
+    console.log('Congrats! You just earned 5 points.');
+    
+}
+
+let alien_Color='Red';
+if(alien_Color=='Green'){
+    console.log("Congratulations! You just earned 5 points.");
+    
+}
