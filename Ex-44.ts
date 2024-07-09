5db00d03ac4fa14f1d38d2c01e4d67285059d387
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-09 12:32:43 +0500

    Add files via upload

---
+++ Ex-44.ts
@@ -0,0 +1,7 @@
+function make_Sandwich(...items:string[]){
+    console.log(`Making a sandwich with the following items: ${items.join(', ')}`)
+}
+
+make_Sandwich('Lettuce', 'Tomato', 'Cheese')
+make_Sandwich('Chicken Spread', 'Spinach')
+make_Sandwich('Peanut Butter', 'mayo', 'mustard sauce')
