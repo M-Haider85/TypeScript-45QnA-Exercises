eb5a719c2ba04e383544d6939903c9f441af52ce
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-09 12:22:16 +0500

    Add files via upload

---
+++ Ex-28.ts
@@ -0,0 +1,32 @@
+let age = 1; 
+
+// Determine the person's stage of life and print the appropriate message
+if (age < 2) {
+    console.log("The person is a baby.");
+} else if (age >= 2 && age < 4) {
+    console.log("The person is a toddler.");
+} else if (age >= 4 && age < 13) {
+    console.log("The person is a kid.");
+} else if (age >= 13 && age < 20) {
+    console.log("The person is a teenager.");
+} else if (age >= 20 && age < 65) {
+    console.log("The person is an adult.");
+} else if (age >= 65) {
+    console.log("The person is an elder.");
+}
+
+let personAge=3;
+if (personAge < 2) {
+    console.log("The person is a baby.");
+} else if (personAge >= 2 && age < 4) {
+    console.log("The person is a toddler.");
+} else if (personAge >= 4 && age < 13) {
+    console.log("The person is a kid.");
+} else if (personAge >= 13 && age < 20) {
+    console.log("The person is a teenager.");
+} else if (personAge >= 20 && age < 65) {
+    console.log("The person is an adult.");
+} else if (personAge >= 65) {
+    console.log("The person is an elder.");
+}
+
