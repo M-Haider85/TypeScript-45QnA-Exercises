84c3372ef580e3a989561666c2eecb7d79436c13
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-09 12:21:26 +0500

    Add files via upload

---
+++ Ex-27.ts
@@ -0,0 +1,37 @@
+let alien="Green"
+if(alien=="Green"){
+    console.log("You have earned 5 points.");    
+}
+else if(alien=="Yellow") {
+    console.log("You have earned 10 points.");
+    }
+else if(alien=="Red"){
+        console.log("You have earned 15 points.");
+    }
+
+
+let alienColor="Yellow"
+if(alienColor=="Green"){
+    console.log("You have earned 5 points.");    
+}
+else if(alienColor=="Yellow") {
+    console.log("You have earned 10 points.");
+    }
+else if(alienColor=="Red"){
+        console.log("You have earned 15 points.");
+    }
+
+
+
+
+
+let alien_Color="Red" 
+if(alien_Color=="Green"){
+    console.log("You have earned 5 points.");    
+}
+else if(alien_Color=="Yellow") {
+    console.log("You have earned 10 points.");
+    }
+else if(alien_Color=="Red"){
+        console.log("You have earned 15 points.");
+    }   
