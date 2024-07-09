427849eb24263746d3b54b26e44fecb0eb4beb84
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-09 12:27:00 +0500

    Add files via upload

---
+++ Ex-34.ts
@@ -0,0 +1,8 @@
+let favouritePizzas:string[]=["BBQ", "Mexican", "Pepperoni"]
+
+for(let i=0; i<favouritePizzas.length; i++){
+    console.log(`I like ${favouritePizzas[i]} pizza.`);
+    }
+
+    console.log("I really love pizza!");
+    
