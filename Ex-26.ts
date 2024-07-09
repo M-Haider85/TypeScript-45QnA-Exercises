9522d320730ea5509e9923f7f4d29215f9ae4a81
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-09 12:21:05 +0500

    Add files via upload

---
+++ Ex-26.ts
@@ -0,0 +1,18 @@
+//runs if block
+let alien_Color="Green"
+if(alien_Color=="Green"){
+    console.log("Congrats! You have just earned 5 points for shooting the alien.");
+    }
+    else{
+        "Congratulations! You just earned 10 points."
+    }
+
+//runs else block
+
+    let alienColor="Red"
+if(alienColor=="Green"){
+    console.log("Congrats! You have just earned 5 points for shooting the alien.");
+    }
+    else{
+        console.log("Congratulations! You just earned 10 points.")
+    }
