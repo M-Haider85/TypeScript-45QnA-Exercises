33a20e8716d8f85cc7c801b73f5fa31e87dff1d2
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-09 12:18:36 +0500

    Add files via upload

---
+++ Ex-21.ts
@@ -0,0 +1,13 @@
+let studentInfo:{Name:string, Age:number, RollNo:number, Teacher:string}={
+    Name:"Wahaj",
+    Age:25,
+    RollNo:12345,
+    Teacher:"Okasha Aijaz"
+}
+console.log(studentInfo.Name);
+console.log(studentInfo.Teacher);
+console.log(studentInfo.Age);
+console.log(studentInfo.RollNo);
+
+
+
