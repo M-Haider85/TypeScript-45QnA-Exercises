f867b9068104dc72e0d2ebdd4d89805680573b2b
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-09 12:28:19 +0500

    Add files via upload

---
+++ Ex-36.ts
@@ -0,0 +1,7 @@
+function make_shirt(size:string, message:string){
+    console.log(`Making a "${size}" size t-shirt with the message "${message}" printed on it`)
+    }
+
+make_shirt('Medium', "My DaD is my Hero!")
+make_shirt("Large","Keep Smiling")
+make_shirt("XL","Eat, Sleep, Code")    
